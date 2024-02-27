9697dd0564c8a470d12464ff66d9fda2f5757287
YasharAzizov <yasharhere@gmail.com>
2024-02-27 15:26:19 +0400

    scroll added

--- src/Components/ScrollToTop/ScrollToTop.jsx
+++ src/Components/ScrollToTop/ScrollToTop.jsx
@@ -2,7 +2,8 @@ import { useLayoutEffect } from "react";
 import { useLocation } from "react-router";
 
 const ScrollToTop = ({ children }) => {
-  const pathname = useLocation();
+  const { pathname } = useLocation();
+
   useLayoutEffect(() => {
     document.documentElement.scrollTo(0, 0);
   }, [pathname]);
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import ScrollToTop from "./Components/scrollToTop/index.jsx";
+import ScrollToTop from "./Components/ScrollToTop/ScrollToTop.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
