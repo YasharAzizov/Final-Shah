fa8b21733732bc34706b70a2881a8036e8a6e827
YasharAzizov <yasharhere@gmail.com>
2024-02-27 15:10:52 +0400

    format

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import Routing from "./Components/Routing/index.jsx";
 import ScrollToTop from "./Components/scrollToTop/index.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
